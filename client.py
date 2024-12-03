11105c5c224be7232db2211d99c0ecb361739009
Jade R-L <119926041+jader01@users.noreply.github.com>
2024-12-03 17:31:36 +0100

    debut int graphique tkinter client.py

--- client.py
+++ client.py
@@ -5,6 +5,7 @@
 ###########################################################################
 
 #import la librairie pour la création de la fenetre
+from tkinter.simpledialog import askstring
 from tkinter import *
 
 #import de la librairie pour la gestion de socket
@@ -17,67 +18,34 @@ import socket
 #
 ###########################################################################
 
+#import la librairie pour la création de la fenetre
 
-#On cree une fenetre avec différents parametres :
 fenetre = Tk() #ici on creer la fenetre
 
-#On cree une un bouton avec différentes parametres :
+def show():
+   search = askstring("Input", "Par quel type voulez vous faire une recherche ")
+   print(search)
+
+def printAll():
+    print("insert le print du programme client")
+
+def addArticle():
+    print("insert de l'ajout des article")
+
+menubar = Menu(fenetre)
+
+menu1 = Menu(menubar, tearoff=0)
+menu1.add_command(label="Recherche", command=show)
+menu1.add_command(label="Affichage", command=printAll)
+menu1.add_command(label="Ajouter", command=addArticle)
+menu1.add_separator()
+menubar.add_cascade(label="Action a realiser", menu=menu1)
+
+fenetre.config(menu=menubar)
+
+
+
 boutonfermer = Button(fenetre, text="quitter", command=fenetre.quit)  # ici on creer le bouton avec le texte inter et a quoi il sert
 boutonfermer.pack(side=BOTTOM)
 
-#On creer un LABEL cad un espaces précus pour écrire du texte : 
-label=Label(fenetre, text="recherche")
-label.pack(side=RIGHT)
-
-value = StringVar()
-value.set("insert de la requete")
-entree = Entry(fenetre, width=30)
-entree.pack()
-
-
-#On lance la boucle principale:
 fenetre.mainloop()
-
-
-############################################################################
-#
-#                        Gestion de la connexion
-#
-###########################################################################
-
-#!/usr/bin/env python3
-import socket
-import os
-from _thread import *
-ClientSocket = None
-host = '127.0.0.1'
-port = 9090
-myNumber = 0
-
-def main():
-    ClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ClientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    try:
-        ClientSocket.connect((host, port))
-    except socket.error as e:
-        print(str(e))
-    finally:
-        print("connection au serveur réussi !")
-        myNumber = int(ClientSocket.recv(1024))
-        print("nombre de client reçu : ", myNumber)
-    start_new_thread(threaded_server, (ClientSocket, myNumber))
-    
-    while True:
-        msg = input('') # bloquant les retours => nécessite un thread
-        ClientSocket.send(str.encode(msg))
-        if msg == "quit": # Bogue sur le quit !
-            ClientSocket.send(str.encode(str(myNumber)))
-            break
-
-def threaded_server(connection, num):
-    while True:
-        response = connection.recv(1024)
-        print(response.decode('utf-8'))
-
-if __name__== "__main__":
-    main()
